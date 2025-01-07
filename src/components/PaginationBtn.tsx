c465c6b80e220c16fcb8b40d8b3f2359e8142d9e
DanishExpertizo <muhammad.danish@expertizo.pk>
2025-01-07 18:54:47 +0500

    style: fix pagination button width

--- src/components/PaginationBtn.tsx
+++ src/components/PaginationBtn.tsx
@@ -5,7 +5,7 @@ const PageButton = ({ children, isActive, onClick }: PageButtonInterface) => {
     return (
         <button
             onClick={onClick}
-            className={`h-10 px-4 flex items-center justify-center border rounded-lg group ${isActive
+            className={`h-10 w-14 flex items-center justify-center border rounded-lg group ${isActive
                 ? 'bg-gray-100 text-gray-700 border-gray-300'
                 : 'text-gray-600 hover:bg-gray-50'
                 }`}
