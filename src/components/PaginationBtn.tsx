84e30ce550cb5f2d252a3e4e926b6992c35ca57f
DanishExpertizo <muhammad.danish@expertizo.pk>
2025-01-07 19:24:03 +0500

    style: made pagination responsive

--- src/components/PaginationBtn.tsx
+++ src/components/PaginationBtn.tsx
@@ -5,7 +5,7 @@ const PageButton = ({ children, isActive, onClick }: PageButtonInterface) => {
     return (
         <button
             onClick={onClick}
-            className={`h-11 w-11 flex items-center justify-center border rounded-lg group ${isActive
+            className={`sm:h-11 sm:w-11 h-8 w-8 flex items-center justify-center border rounded-lg group ${isActive
                 ? 'bg-gray-100 text-gray-700 border-gray-300'
                 : 'text-gray-600 hover:bg-gray-50'
                 }`}
