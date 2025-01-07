c3a1966c76b5b13dd30594a33eb04dde3b739f02
DanishExpertizo <muhammad.danish@expertizo.pk>
2025-01-07 16:28:53 +0500

    fix: modified pagination and used dynamic api for dynamic data with SSG Pagination

--- src/app/product/[product]/page.tsx
+++ src/app/product/[product]/page.tsx
@@ -1,6 +1,12 @@
 import SingleProduct from '@/views/SingleProductView';
 import { ProductInterface } from '@/interface/interface';
 import { getSinglePro } from '@/utils/getSinglePro';
+import { getTotalNum } from '@/utils/getTotalNum';
+
+export async function generateStaticParams() {
+    const { totalProducts } = await getTotalNum();
+    return Array.from({ length: totalProducts }, (_, i) => ({ product: (i + 1).toString() }));
+}
 
 const Product = async ({ params }: ProductInterface) => {
     const { product } = params;
--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -6,8 +6,8 @@ export async function getData(page: number) {
     const res = await fetch(`https://dummyjson.com/products/?limit=${postsPerPage}&skip=${(page - 1) * postsPerPage}`, { next: { revalidate: 3600 } });
     if (!res.ok) throw new Error('Failed to fetch data');
     const data = await res.json();
-    
-    const totalPages = await getTotalNum();
+
+    const { totalPages } = await getTotalNum();
 
     return {
         products: data.products,
